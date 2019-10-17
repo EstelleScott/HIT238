ae3c10e3bed859404801245499bc6e3b9f0c26d6
estelles <estiealysse@hotmail.com>
2019-10-18 02:00:50 +0930

    -added functionality to the button using JS (it works now yay)

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 //final grade equation interpreted from:
 //https://www.rapidtables.com/calc/grade/final-grade-calculation.html
+
+function calculate(){
+  //get values from input boxes
+  var currentGrade = Number(document.getElementById("currentgrade").value);
+  var desiredGrade = Number(document.getElementById("desiredgrade").value);
+  var finalWeight = Number(document.getElementById("finalweight").value);
+  // percentage needed calculation
+  var gradeNeeded = ((1*(desiredGrade/100)-(1-(finalWeight/100))*(currentGrade/100))/(finalWeight/100))*100;
+  //if the variables do not result in NaN, display reult
+  if(!isNaN(gradeNeeded))
+  {
+    //rounds number to whole percentage
+    var rounded = Math.round(gradeNeeded)
+    //display result in label
+    document.getElementById("result").innerHTML = "Your grade needed is " + rounded + "%";
+  }
+}
